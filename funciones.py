90d80308da44e6f256dc5e5722eb66782c3b00c0
MarioFelipe19 <felipeparra257@hotmail.com>
2024-05-09 03:33:32 -0500

    Feat : arreglo menu

--- funciones.py
+++ funciones.py
@@ -1,4 +1,4 @@
-def registrar_usuario():
+def agregar_usuario():
     print("hola1")
 
 def buscar_usuario():
@@ -10,17 +10,62 @@ def eliminar_usuario():
 def modificar_usuario():
     print("hola4")
 
-def consultar_servicio():
-    print("hola5")
+def agregar_producto():
+    print("hola13")
+
+def buscar_producto():
+    print("hola14")
+
+def eliminar_producto():
+    print("hola15")
+
+def modificar_producto():
+    print("hola16")
+
+def agregar_servicio():
+    print("hola17")
+
+def buscar_servicio():
+    print("hola18")
+
+def eliminar_servicio():
+    print("hola19")
+
+def modificar_servicio():
+    print("hola20")
 
 def agregar_reclamacion():
+    print("hola5")
+
+def buscar_reclamacion():
     print("hola6")
 
-def agregar_sugerencia():
+def eliminar_reclamacion():
     print("hola7")
 
-def registrar_producto():
+def modificar_reclamacion():
     print("hola8")
 
-def registrar_venta_producto():
+def agregar_sugerencia():
     print("hola9")
+
+def buscar_sugerencia():
+    print("hola10")
+
+def eliminar_sugerencia():
+    print("hola11")
+
+def modificar_sugerencia():
+    print("hola12")
+
+def agregar_venta():
+    print("hola21")
+
+def buscar_venta():
+    print("hola22")
+
+def eliminar_venta():
+    print("hola23")
+
+def modificar_venta():
+    print("hola24")
--- main.py
+++ main.py
@@ -6,32 +6,102 @@ while True:
     op = pedir_opcion()
     if op == 1:
         print("--"*20)
-        registrar_usuario()
+        agregar_usuario()
+        print("--"*20)
     elif op == 2:
         print("--"*20)
         buscar_usuario()
+        print("--"*20)
     elif op == 3:
         print("--"*20)
         eliminar_usuario()
+        print("--"*20)
     elif op == 4:
         print("--"*20)
         modificar_usuario()
+        print("--"*20)
     elif op == 5:
         print("--"*20)
-        consultar_servicio()
+        agregar_producto()
+        print("--"*20)
     elif op == 6:
         print("--"*20)
-        agregar_reclamacion()
+        buscar_producto()
+        print("--"*20)
     elif op == 7:
         print("--"*20)
-        agregar_sugerencia()
+        eliminar_producto()
+        print("--"*20)
     elif op == 8:
         print("--"*20)
-        registrar_producto
+        modificar_producto()
+        print("--"*20)
     elif op == 9:
         print("--"*20)
-        registrar_venta_producto
+        agregar_servicio()
+        print("--"*20)
     elif op == 10:
         print("--"*20)
-        print("Salió!!")
+        buscar_servicio()
+        print("--"*20)
+    elif op == 11:
+        print("--"*20)
+        eliminar_servicio()
+        print("--"*20)
+    elif op == 12:
+        print("--"*20)
+        modificar_servicio()
+        print("--"*20)
+    elif op == 13:
+        print("--"*20)
+        agregar_reclamacion()
+        print("--"*20)
+    elif op == 14:
+        print("--"*20)
+        buscar_reclamacion()
+        print("--"*20)
+    elif op == 15:
+        print("--"*20)
+        eliminar_reclamacion()
+        print("--"*20)
+    elif op == 16:
+        print("--"*20)
+        modificar_reclamacion()
+        print("--"*20)
+    elif op == 17:
+        print("--"*20)
+        agregar_sugerencia()
+        print("--"*20)
+    elif op == 18:
+        print("--"*20)
+        buscar_sugerencia()
+        print("--"*20)
+    elif op == 19:
+        print("--"*20)
+        eliminar_sugerencia()
+        print("--"*20)
+    elif op == 20:
+        print("--"*20)
+        modificar_sugerencia()
+        print("--"*20)
+    elif op == 21:
+        print("--"*20)
+        agregar_venta()
+        print("--"*20)
+    elif op == 22:
+        print("--"*20)
+        buscar_venta()
+        print("--"*20)
+    elif op == 23:
+        print("--"*20)
+        eliminar_venta()
+        print("--"*20)
+    elif op == 24:
+        print("--"*20)
+        modificar_venta()
+        print("--"*20)
+    elif op == 25:
+        print("--"*20)
+        print("Adios!!")
+        print("--"*20)
         break
